f8f64136f6d1e5a3f7c94fc03f8529e2e8512daf
borland502 <jhettenh@gmail.com>
2024-12-26 15:28:07 -0500

    Fused nx and angular plugins into a single monorepo

---
+++ apps/watchyourlan-services/src/main/java/com/technohouser/watchyourlanservices/WatchyourlanServicesApplication.java
@@ -0,0 +1,13 @@
+package com.technohouser.watchyourlanservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchyourlanServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchyourlanServicesApplication.class, args);
+	}
+
+}
---
+++ apps/watchyourlan-services/src/test/java/com/technohouser/watchyourlanservices/WatchyourlanServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technohouser.watchyourlanservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchyourlanServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
