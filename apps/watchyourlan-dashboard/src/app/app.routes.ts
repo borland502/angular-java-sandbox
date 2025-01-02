19ceacc57486665d3041199aa0919c8d1ec7827a
borland502 <jhettenh@gmail.com>
2025-01-02 17:17:52 -0500

    Ng-matero updates for angular 19 to login and registration

--- apps/watchyourlan-dashboard/src/app/app.routes.ts
+++ apps/watchyourlan-dashboard/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { authGuard } from '@core';
+import { AdminLayoutComponent } from '@theme/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from '@theme/auth-layout/auth-layout.component';
+import { LoginComponent } from '@ng-matero/routes/sessions/login/login.component';
+import { RegisterComponent } from '@ng-matero/routes/sessions/register/register.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: AdminLayoutComponent,
+    canActivate: [authGuard],
+    canActivateChild: [authGuard],
+    children: [],
+  },
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+    ],
+  },
+  { path: '**', redirectTo: 'dashboard' },
+];
