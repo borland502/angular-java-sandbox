9524d38a665678ec5cdced31a6ae8ab955138da1
borland502 <jhettenh@gmail.com>
2025-01-06 16:17:00 -0500

    Removed ng-matero for now and got a basic material table working with sqlite

--- apps/watchyourlan-dashboard/src/app/app.component.ts
+++ apps/watchyourlan-dashboard/src/app/app.component.ts
@@ -1,14 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { AfterViewInit, Component, inject } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatButton } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { EntityModelHost } from '@technohouser/api/models';
+import {
+  HostEntityControllerService
+} from '@technohouser/api/services/host-entity-controller/host-entity-controller.service';
 
 @Component({
-    imports: [RouterModule, MatToolbar, MatButton],
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
+  imports: [
+    RouterModule,
+    MatToolbar,
+    MatButton,
+    MatTableModule,
+    RouterLink
+  ],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true
 })
-export class AppComponent {
-    title = 'watchyourlan-dashboard';
+export class AppComponent implements AfterViewInit {
+
+  resultsLength = 0;
+  isLoadingResults = true;
+  isRateLimitReached = false;
+  title = 'watchyourlan-dashboard';
+  displayedColumns: string[] = [
+    'name',
+    'ip',
+    'mac',
+    'date',
+    'hw',
+    'known',
+    'now'
+  ];
+  protected hosts: EntityModelHost[] = [];
+  private hostDataService = inject(HostEntityControllerService);
+
+  ngAfterViewInit(): void {
+    this.hostDataService
+      .getCollectionResourceHostGet()
+      .subscribe((response) => {
+        console.log(response);
+        if (typeof response !== 'string' && response._embedded?.hosts) {
+          this.hosts = response._embedded.hosts;
+        } else {
+          console.error('Error loading hosts');
+        }
+      });
+  }
+
 }
--- apps/watchyourlan-dashboard/src/app/components/auth/login/login.component.ts
+++ apps/watchyourlan-dashboard/src/app/components/auth/login/login.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-login',
-    imports: [CommonModule],
-    templateUrl: './login.component.html',
-    styleUrl: './login.component.css',
+  selector: 'app-login',
+  imports: [CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+  standalone: true
 })
 export class LoginComponent {}
